6eaf4d7cd4ed718557e5ca1c03895efdcc93b33e
Oshiina <nicolaslabbe2000@gmail.com>
2020-03-30 00:06:51 +0200

    Create StartWindow
    Split "start" method in another class

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -7,19 +7,7 @@ import org.json.simple.JSONObject;
 
 import games.Game;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -27,44 +15,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
 
-		BorderPane root = new BorderPane();
+		StartWindow root = new StartWindow();
 		Scene scene = new Scene(root);
 		
-		Group gr = new Group();
-		root.setCenter(gr);
-
-		VBox vb = new VBox();
-		Text txt = new Text(10,40,"Create a new or open an existing one");
-		txt.setFont(new Font(40));
-		HBox hb = new HBox();
-		Button b1 = new Button("New");
-		Button b2 = new Button("Open");
-		
-		vb.getChildren().addAll(txt,hb);
-		hb.getChildren().addAll(b1,b2);
-		gr.getChildren().addAll(vb);
-		
-
-		MenuBar menuBar = new MenuBar();
-		root.setTop(menuBar);
-		Menu fileMenu = new Menu("File");
-		menuBar.getMenus().addAll(fileMenu);
-
-		MenuItem newItem = new MenuItem("New");
-		MenuItem openItem = new MenuItem("Open");
-		
-		EventHandler<ActionEvent> eventOpen = new EventHandler<ActionEvent>() { 
-            public void handle(ActionEvent e) 
-            { 
-               System.out.println("cc");
-            } 
-        }; 
-        
-        b2.setOnAction(eventOpen);
-        openItem.setOnAction(eventOpen);
-
-		fileMenu.getItems().addAll(newItem, openItem);
-
 		stage.setTitle("GamesLib");
 		stage.setScene(scene);
 		stage.show();
---
+++ src/ui/StartWindow.java
@@ -0,0 +1,59 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+public class StartWindow extends BorderPane{
+
+	public StartWindow() {
+		super();
+		
+		Group gr = new Group();
+		setCenter(gr);
+
+		VBox vb = new VBox();
+		Text txt = new Text(10,40,"Create a new or open an existing one");
+		txt.setFont(new Font(40));
+		HBox hb = new HBox();
+		Button b1 = new Button("New");
+		Button b2 = new Button("Open");
+		
+		vb.getChildren().addAll(txt,hb);
+		hb.getChildren().addAll(b1,b2);
+		gr.getChildren().addAll(vb);
+		
+
+		MenuBar menuBar = new MenuBar();
+		setTop(menuBar);
+		Menu fileMenu = new Menu("File");
+		menuBar.getMenus().addAll(fileMenu);
+
+		MenuItem newItem = new MenuItem("New");
+		MenuItem openItem = new MenuItem("Open");
+		
+
+        
+        b2.setOnAction(eventOpen);
+        openItem.setOnAction(eventOpen);
+
+		fileMenu.getItems().addAll(newItem, openItem);
+	}
+	
+	EventHandler<ActionEvent> eventOpen = new EventHandler<ActionEvent>() { 
+        public void handle(ActionEvent e) 
+        { 
+           System.out.println("cc");
+        } 
+    };
+	
+}
