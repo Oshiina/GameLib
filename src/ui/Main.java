d2ec76a92bd0bc9e3bc7ebee7e356aceac0bf72f
Oshiina <nicolaslabbe2000@gmail.com>
2020-03-29 22:55:23 +0200

    Event on menu

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -7,9 +7,12 @@ import org.json.simple.JSONObject;
 
 import games.Game;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -22,17 +25,30 @@ public class Main extends Application {
 
 		BorderPane root = new BorderPane();
 		Scene scene = new Scene(root);
-		
-		
+
 		Text text = new Text(10, 40, "Hello World!");
 		text.setFont(new Font(40));
 		root.setCenter(text);
-		
+
 		MenuBar menuBar = new MenuBar();
 		root.setTop(menuBar);
 		Menu fileMenu = new Menu("File");
-		menuBar.getMenus().add(fileMenu);
+		menuBar.getMenus().addAll(fileMenu);
+
+		MenuItem newItem = new MenuItem("New");
+		MenuItem openItem = new MenuItem("Open");
 		
+		EventHandler<ActionEvent> eventOpen = new EventHandler<ActionEvent>() { 
+            public void handle(ActionEvent e) 
+            { 
+               System.out.println("cc");
+            } 
+        }; 
+        
+        openItem.setOnAction(eventOpen);
+
+		fileMenu.getItems().addAll(newItem, openItem);
+
 		stage.setTitle("GamesLib");
 		stage.setScene(scene);
 		stage.show();
